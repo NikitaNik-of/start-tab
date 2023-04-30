6fd6ceb9ecc2863148ecd4d13b9638cfb050a0e7
NikitaNik_of <ncayka@gmail.com>
2023-05-01 01:54:13 +0300

    initial version

--- pages/index.js
+++ pages/index.js
@@ -1,117 +1,119 @@
 import Image from 'next/image'
+import topimg from "../public/images/topimg.jpg"
 import { Inter } from 'next/font/google'
+import { TbArrowUpRight, TbPlayerPlay, TbSearch, TbPhotoSearch } from 'react-icons/tb'
+import { useState } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
+
+
 
 export default function Home() {
+  const [icon, setIcon] = useState(0)
+  const [search, setSearch] = useState("")
+  const [bar, setBar] = useState("focus:shadow-gray-500/40")
+  const [cl, setCl] = useState(["", "translate-y-9 opacity-0", "translate-y-9 opacity-0", "translate-y-9 opacity-0"])
+
+  const sendSearch = () => {
+    if (search[0] == "!"){
+      window.location.href = "https://" + search.substring(1)
+    }
+    else if (search[0] == "@"){
+      window.location.href = "https://www.youtube.com/results?search_query=" + search.substring(1)
+    }
+    else if (search[0] == "#"){
+      window.location.href = "https://yandex.ru/images/search?text=" + search.substring(1)
+    }
+    else {
+      window.location.href = "https://yandex.ru/search/?text=" + search
+    }
+  }
+
+  const check = (e) => {
+    if (e.target.value[0] != ' ') {
+      setSearch(e.target.value)
+      if (e.target.value[0] == "!"){
+        changeIcon(1)
+      }
+      else if (e.target.value[0] == "@"){
+        changeIcon(2)
+      }
+      else if (e.target.value[0] == "#"){
+        changeIcon(3)
+      }
+      else {
+        changeIcon(0)
+      }
+    }
+  }
+
+  const input = (event) => {
+    if(event.key === 'Enter'){
+      sendSearch()
+      console.log('enter press here! ')
+    }
+    else if(event.key === 'Escape'){
+      setBar("focus:shadow-red-900/40")
+      setTimeout(() => {
+        setBar("focus:shadow-gray-500/40")
+      }, 500);
+      setSearch("")
+    }
+  }
+
+  const changeIcon = (key) => {
+    if (key != icon){
+      var cArray = ["translate-y-9 opacity-0", "translate-y-9 opacity-0", "translate-y-9 opacity-0", "translate-y-9 opacity-0"]
+      cArray[key] = " "
+      cArray[icon] = "-translate-y-9 opacity-0"
+      setCl(cArray)
+      setTimeout(() => {
+        cArray[icon] = "translate-y-9 opacity-0"
+        setCl(cArray)
+        setIcon(key)
+      }, 500);
+    }
+  }  
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-center p-24`}
     >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className='w-[700px] grow flex flex-col justify-around'>
+        <div>
+          <div className='relative h-48 mb-5'>
+            <text className='absolute bottom-[2px] left-1 text-[14px] text-gray-600 italic'>{"Валаамский монастырь. Никольский скит. 9 августа 2022"}</text>
+            <Image alt='' src={topimg} className='absolute h-48 object-cover rounded-xl duration-500 hover:-translate-y-6 transition-all'/>
+          </div>
+          <div className='flex space-x-1'>
+            <input
+              onKeyDown={input}
+              onChange={check}
+              value={search}
+              autoComplete="off"
+              placeholder='найди что-нибудь!'
+              className={bar + ' w-full bg-gray-800 rounded-xl py-2 px-4 placeholder:text-gray-600 focus:outline-none hover:shadow-lg hover:shadow-gray-600/50 focus:shadow-xl transition-shadow'}/>
+            <button onClick={sendSearch} className='overflow-hidden flex items-center justify-center bg-cyan-950 hover:bg-cyan-800 rounded-xl p-2 hover:shadow-lg hover:shadow-cyan-500/40 transition-all group duration-500'>
+              <div className='flex items-center justify-center relative h-7 w-7'>
+                <TbSearch className={cl[0] + " absolute h-7 w-7 group-hover:-rotate-12 transition-all duration-500 "}/>
+                <TbArrowUpRight className={cl[1] + " absolute h-7 w-7 group-hover:translate-x-[2px] group-hover:-translate-y-[2px] transition-all duration-500 "}/>
+                <TbPlayerPlay className={cl[2] + " absolute h-7 w-7 group-hover:translate-x-[2px] transition-all duration-500 "}/>
+                <TbPhotoSearch className={cl[3] + " absolute h-7 w-7 group-hover:scale-110 transition-all duration-500 "}/>
+              </div>
+            </button>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className='text-center'>
+          <div className='text-sm text-cyan-500 pt-16 pb-1'>Начни поиск с символа...</div>
+          <div className='text-[14px] text-gray-500 flex justify-around'>
+            <button className='hover:text-gray-400' onClick={() => {setSearch("!" + search)}}><text className='text-gray-200'>!</text>{" открыть ссылку"}</button>
+            <button className='hover:text-gray-400' onClick={() => {setSearch("#" + search)}}><text className='text-gray-200'>#</text>{" искать в Яндекс.Картинки"}</button>
+            <button className='hover:text-gray-400' onClick={() => {setSearch("@" + search)}}><text className='text-gray-200'>@</text>{" искать в Youtube"}</button>
+          </div>
+        </div>
+        {/* <button onClick={() => {changeIcon(0)}} className='w-12 h-12 bg-cyan-950 hover:bg-cyan-800 rounded-xl p-2 hover:shadow-lg hover:shadow-cyan-500/40 transition-all group duration-300'>{"0"}</button>
+        <button onClick={() => {changeIcon(1)}} className='w-12 h-12 bg-cyan-950 hover:bg-cyan-800 rounded-xl p-2 hover:shadow-lg hover:shadow-cyan-500/40 transition-all group duration-300'>{"1"}</button>
+        <button onClick={() => {changeIcon(2)}} className='w-12 h-12 bg-cyan-950 hover:bg-cyan-800 rounded-xl p-2 hover:shadow-lg hover:shadow-cyan-500/40 transition-all group duration-300'>{"2"}</button>
+        <button onClick={() => {changeIcon(3)}} className='w-12 h-12 bg-cyan-950 hover:bg-cyan-800 rounded-xl p-2 hover:shadow-lg hover:shadow-cyan-500/40 transition-all group duration-300'>{"3"}</button> */}
       </div>
     </main>
   )
